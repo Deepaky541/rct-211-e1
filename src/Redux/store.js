f1fe6484a5520c0a36e0c43bc03fd8b5f76942ad
Deepak yadav <deepaky541.dy@gmail.com>
2022-06-27 12:27:22 +0530

    first commit

---
+++ src/Redux/store.js
@@ -0,0 +1,12 @@
+// TODO: use this store variable to create a store.
+
+import { legacy_createStore } from "redux";
+import { reducer as shoeReducer } from "./reducer";
+
+const store = legacy_createStore(shoeReducer);
+export { store };
+
+// NOTE: Do not remove this code,its used for calculating your score, if removed it will give you zero marks
+if (window.Cypress) {
+  window.store = store;
+}
